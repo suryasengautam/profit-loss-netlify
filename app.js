207dab8676b2a8bb3d684fabee69dde3cacf9141
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-11 10:59:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// selecting all elements
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#quantity-of-stock");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+submitBtn.addEventListener("click",submitHanler)
+function submitHanler(){
+  var ip = Number(initialPrice.value)
+  var qty = Number(stockQuantity.value);
+  var curr = Number(currentPrice.value);
+  calculateProfitAndLoss(ip,qty,curr)
+}
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    showOutput(
+      `hey the profit is ${loss} and the percentage loss is ${lossPercentage}%`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    showOutput(
+      `hey the profit is ${profit} and the percentage loss is ${profitPercentage}%`
+    );
+    
+  } else {
+    showOutput("equal");
+
+  }
+}
+function showOutput(message){
+  outputBox.innerHTML = message
+
+}
