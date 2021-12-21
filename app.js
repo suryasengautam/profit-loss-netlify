f2ec73c40590470bf15b55e9fd3ed27f44ae69a5
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-21 17:53:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ function submitHanler(){
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+    var lossPercentage = (loss / ( initial*quantity)) * 100;
     showOutput(
       `hey the loss is ${loss} and the percentage loss is ${lossPercentage}%`
     );
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    var profitPercentage = (profit /( initial*quantity)) * 100;
     showOutput(
       `hey the profit is ${profit} and the percentage profit is ${profitPercentage}%`
     );
